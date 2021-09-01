b18482d67d14668cf6eb173546ea8b09ffac07c2
Vinicius Antoniolli <88800074+vinicius-antoniolli@users.noreply.github.com>
2021-09-01 01:43:36 -0300

    Update and rename AlunoResetLeituraTeclado.java to aula06-AlunoResetLeituraTeclado.java

--- exs-02-java/aula06-AlunoResetLeituraTeclado.java
+++ exs-02-java/aula06-AlunoResetLeituraTeclado.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class AlunoResetLeituraTeclado {
+public class AlunoResetLeituraTeclado {
 
     public static void main(String[] args) {
         System.out.println("Digite seu nome completo: ");
