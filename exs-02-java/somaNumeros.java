c827344d07064f9952163f38d251828fa0623bee
Vinicius Antoniolli <88800074+vinicius-antoniolli@users.noreply.github.com>
2021-08-31 21:57:22 -0700

    Add files via upload

---
+++ exs-02-java/somaNumeros.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class somaNumeros {
+    public static void main(String[] args) {
+        Scanner inserirNumeros = new Scanner(System.in);
+
+        System.out.println("Insira o primeiro numero:");
+        int numero1 = inserirNumeros.nextInt();
+        System.out.println("Insira o segundo numero:");
+        int numero2 = inserirNumeros.nextInt();
+        int resultado = numero1 + numero2;
+        System.out.println("A soma dos dois valores é: " + resultado);
+    }
+}
