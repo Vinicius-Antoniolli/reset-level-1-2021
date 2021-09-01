853116deef95cb937cd6c00971cdde45393f1b10
Vinicius Antoniolli <88800074+vinicius-antoniolli@users.noreply.github.com>
2021-09-01 06:17:14 -0300

    Update and rename Aula10ex1.java to aula10-ex1-Aula10ex1.java

--- exs-02-java/aula10-ex1-Aula10ex1.java
+++ exs-02-java/aula10-ex1-Aula10ex1.java
@@ -26,3 +26,28 @@ public class Aula10ex1 {
 
     }
 }
+
+
+
+Insira o 1o valor:
+10
+Insira o 2o valor:
+20
+Insira o 3o valor:
+30
+Insira o 4o valor:
+40
+Insira o 5o valor:
+50
+Valores do primeiro vetor: 
+10.0
+20.0
+30.0
+40.0
+50.0
+Valores do segundo vetor: 
+20.0
+40.0
+60.0
+80.0
+100.0
