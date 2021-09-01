cb61fef5961ecd39b0817161ce7e0fa8ac316be7
Vinicius Antoniolli <88800074+vinicius-antoniolli@users.noreply.github.com>
2021-08-31 21:47:09 -0700

    Add files via upload

---
+++ exs-02-java/AlunoResetLeituraTeclado.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AlunoResetLeituraTeclado {
+
+    public static void main(String[] args) {
+        System.out.println("Digite seu nome completo: ");
+        Scanner inserirNome = new Scanner(System.in);
+        String nomeCompleto = inserirNome.nextLine();
+        System.out.println("\nSou aluno do Level 1 do CWI Reset! Me chamo " + nomeCompleto);
+    }
+}
