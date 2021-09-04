9fe20ec819f6738a828eb18b98508225d5115cad
Vinicius Antoniolli <88800074+vinicius-antoniolli@users.noreply.github.com>
2021-09-04 03:19:19 -0300

    Update and rename Personagem.java to aula03-ex1-Personagem.java

--- exs-03-orientacao-a-objetos/aula03-ex1-Personagem.java
+++ exs-03-orientacao-a-objetos/aula03-ex1-Personagem.java
@@ -3,7 +3,7 @@ package Aula2Mario;
 public class Personagem {
     public static void main(String[] args) {
         Aula2Mario.Mario h1 = new Aula2Mario.Mario();
-        h1.nome = "Mario3 Bros";
+        h1.nome = "Mario Bros";
         // h1.altura = 1.50; (NÃO PODE MAIS ALTERAR POR SER PRIVADO)
         h1.corDaPele = "Clara";
         h1.corDosOlhos = "Azuis";
@@ -18,3 +18,16 @@ public class Personagem {
         //h1.imprimeSuperPoderes(); (NÃO PODE MAIS IMPRIMIR POR SER PRIVADO)
     }
 }
+
+
+
+Nome: Mario Bros
+Altura: 0.0
+Idade: 0
+Cor da Pele: Clara
+Cor dos Olhos: Azuis
+Tamanho do nariz: Grande
+Cor da camiseta:  Vermelha
+Ele tem bigode?: false
+Ele usa costeletas? true
+Está de boné? true
