6a62db8803c316a71222ca7c3ff57b7f894d25ea
Vinicius Antoniolli <88800074+vinicius-antoniolli@users.noreply.github.com>
2021-09-04 02:31:37 -0300

    Update and rename Personagem.java to aula02-ex1-Personagem.java

--- exs-03-orientacao-a-objetos/aula02-ex1-Personagem.java
+++ exs-03-orientacao-a-objetos/aula02-ex1-Personagem.java
@@ -17,3 +17,15 @@ public class Personagem {
         h1.imprimeSuperPoderes();
     }
 }
+
+
+Nome: Mario Bros
+Altura: 1.5
+Cor da Pele: Clara
+Cor do Olhos: Azuis
+Tamanho do nariz: Grande
+Cor da camiseta:  Vermelha
+Ele tem bigode?: true
+Ele usa costeletas? true
+Está de boné? true
+Super salto, respirar em baixo d'água, teleporte, comunicação telepática com Luigi, manipulação de tamanho
